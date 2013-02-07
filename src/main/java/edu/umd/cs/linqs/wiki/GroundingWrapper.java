84d2012596cb0f076dd7cce5bfdf4d7e6f6e87cd
Bert Huang <bert@cs.umd.edu>
2013-02-07 17:33:27 -0500

    Made GroundingWrapper implement Comparable

--- src/main/java/edu/umd/cs/linqs/wiki/GroundingWrapper.java
+++ src/main/java/edu/umd/cs/linqs/wiki/GroundingWrapper.java
@@ -3,7 +3,7 @@ package edu.umd.cs.linqs.wiki;
 import edu.umd.cs.psl.model.argument.GroundTerm;
 
 
-public class GroundingWrapper {
+public class GroundingWrapper implements Comparable<GroundingWrapper> {
 	private GroundTerm [] grounding;
 
 	public GroundingWrapper(GroundTerm [] args) {
@@ -29,6 +29,24 @@ public class GroundingWrapper {
 		for (int i = 0; i < grounding.length; i++)
 			code += 13*i*grounding[i].hashCode();
 		return code;
+	}
+	
+	
+
+	@Override
+	public int compareTo(GroundingWrapper oth) {
+		if (grounding.length < oth.grounding.length)
+			return -1;
+		else if (grounding.length > oth.grounding.length)
+			return 1;
+		else {
+			for (int i = 0; i < grounding.length; i++) {
+				int elementCompare = grounding[i].compareTo(oth.grounding[i]);
+				if (elementCompare != 0)
+					return elementCompare;
+			}
+		}
+		return 0;
 	}
 
 }
