88a8e6811c1f1817fe4633095fcfb8f1afa6d118
Ben London <blondon@asbury.umiacs.umd.edu>
2013-04-13 15:15:42 -0400

    added data loader and other changes

---
+++ src/main/java/edu/umd/cs/linqs/action/Action1_DataLoader.groovy
@@ -0,0 +1,91 @@
+package edu.umd.cs.linqs.action
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import edu.umd.cs.psl.config.*
+import edu.umd.cs.psl.database.DataStore
+import edu.umd.cs.psl.database.Partition
+import edu.umd.cs.psl.database.loading.Inserter
+import edu.umd.cs.psl.database.rdbms.RDBMSDataStore
+import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver
+import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver.Type
+import edu.umd.cs.psl.groovy.*
+import edu.umd.cs.psl.model.argument.ArgumentType
+import edu.umd.cs.psl.ui.loading.*
+
+
+/*** CONFIGURATION PARAMETERS ***/
+
+Logger log = LoggerFactory.getLogger(this.class)
+ConfigManager cm = ConfigManager.getManager();
+ConfigBundle cb = cm.getBundle("action");
+
+def defPath = "data/action/action";// System.getProperty("java.io.tmpdir") + "/action"
+def dbpath = cb.getString("dbpath", defPath)
+DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, true), cb)
+
+int numSeqs = 44;
+
+
+/*** KB DEFINITION ***/
+
+log.info("Initializing KB ...");
+
+PSLModel m = new PSLModel(this, data);
+
+/* PREDICATES */
+
+// target
+m.add predicate: "doing", types: [ArgumentType.UniqueID,ArgumentType.Integer];
+m.add predicate: "sameObj", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
+
+// observed
+m.add predicate: "inFrame", types: [ArgumentType.UniqueID,ArgumentType.Integer,ArgumentType.Integer];
+m.add predicate: "inSameFrame", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
+m.add predicate: "inSeqFrames", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
+m.add predicate: "dims", types: [ArgumentType.UniqueID,ArgumentType.Integer,ArgumentType.Integer,ArgumentType.Integer,ArgumentType.Integer];
+m.add predicate: "hogAction", types: [ArgumentType.UniqueID,ArgumentType.Integer];
+m.add predicate: "acdAction", types: [ArgumentType.UniqueID,ArgumentType.Integer];
+
+
+/** DATASTORE PARTITIONS **/
+
+int partCnt = 0;
+Partition[][] partitions = new Partition[2][numSeqs];
+for (int s = 0; s < numSeqs; s++) {
+	partitions[0][s] = new Partition(partCnt++);	// observations
+	partitions[1][s] = new Partition(partCnt++);	// labels
+}
+	
+	
+/*** LOAD DATA ***/
+
+log.info("Loading data ...");
+
+def dataPath = "./data/action/"
+def filePfx = dataPath + "d1_";
+
+Inserter[] inserters;
+
+/* Ground truth */
+inserters = InserterUtils.getMultiPartitionInserters(data, doing, partitions[1], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "action.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, sameObj, partitions[1], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "sameObj.txt");
+
+/* Observations */
+inserters = InserterUtils.getMultiPartitionInserters(data, inFrame, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "inframe.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, inSameFrame, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "insameframe.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, inSeqFrames, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "inseqframes.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, dims, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "coords.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, hogAction, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataTruthMultiPartition(inserters, filePfx + "hogaction.txt");
+inserters = InserterUtils.getMultiPartitionInserters(data, acdAction, partitions[0], numSeqs);
+InserterUtils.loadDelimitedDataTruthMultiPartition(inserters, filePfx + "acdaction.txt");
+
+log.info("Done!");
--- src/main/java/edu/umd/cs/linqs/action/ActionRecog1.groovy
+++ src/main/java/edu/umd/cs/linqs/action/ActionRecog1.groovy
@@ -15,16 +15,14 @@ import edu.umd.cs.psl.database.DataStore
 import edu.umd.cs.psl.database.Database
 import edu.umd.cs.psl.database.DatabasePopulator
 import edu.umd.cs.psl.database.Partition
-import edu.umd.cs.psl.database.loading.Inserter
 import edu.umd.cs.psl.database.rdbms.RDBMSDataStore
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver
 import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver.Type
 import edu.umd.cs.psl.evaluation.result.FullInferenceResult
-import edu.umd.cs.psl.evaluation.statistics.DiscretePredictionStatistics
+import edu.umd.cs.psl.evaluation.statistics.ConfusionMatrix
 import edu.umd.cs.psl.evaluation.statistics.MulticlassPredictionComparator
 import edu.umd.cs.psl.evaluation.statistics.MulticlassPredictionStatistics
 import edu.umd.cs.psl.groovy.*
-import edu.umd.cs.psl.model.argument.ArgumentType
 import edu.umd.cs.psl.model.argument.GroundTerm
 import edu.umd.cs.psl.model.argument.IntegerAttribute
 import edu.umd.cs.psl.model.argument.Variable
@@ -36,41 +34,43 @@ import edu.umd.cs.psl.model.parameters.Weight
 import edu.umd.cs.psl.ui.loading.*
 import edu.umd.cs.psl.util.database.Queries
 
-/*** CONFIGURATION PARAMETERS ***/
 
-def dataPath = "./data/action/"
-def filePfx = dataPath + "d1_";
+//ConfusionMatrix cmat = new ConfusionMatrix(3);
+//cmat.set(0,0,500);
+//cmat.set(0,1,300);
+//cmat.set(0,2,200);
+//cmat.set(1,1,200);
+//cmat.set(2,1,400);
+//System.out.println(cmat.toMatlabString());
+//SquareMatrix pmat = cmat.getPrecisionMatrix();
+//System.out.println(pmat);
+//SquareMatrix rmat = cmat.getRecallMatrix();
+//System.out.println(rmat);
+//System.out.println(SquareMatrix.average([pmat,rmat]).toMatlabString(5))
+//return 0;
+
+/*** CONFIGURATION PARAMETERS ***/
 
 Logger log = LoggerFactory.getLogger(this.class)
 ConfigManager cm = ConfigManager.getManager();
 ConfigBundle cb = cm.getBundle("action");
 
-def defPath = System.getProperty("java.io.tmpdir") + "/action"
+def defPath = "data/action/action";// System.getProperty("java.io.tmpdir") + "/action"
 def dbpath = cb.getString("dbpath", defPath)
-DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, true), cb)
-folds = 44
+DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, false), cb)
+
+int numSeqs = 44;
 
 def sq = cb.getBoolean("squared", true);
 
+
+/** EXPERIMENT CONFIG **/
+
 ExperimentConfigGenerator configGenerator = new ExperimentConfigGenerator("action");
 
-/*
- * SET MODEL TYPES
- *
- * Options:
- * "quad" HLEF
- * "bool" MLN
- */
 configGenerator.setModelTypes(["quad"]);
 
-/*
- * SET LEARNING ALGORITHMS
- *
- * Options:
- * "MLE" (MaxLikelihoodMPE)
- * "MPLE" (MaxPseudoLikelihood)
- * "MM" (MaxMargin)
- */
+/* Learning methods */
 //methods = ["MLE","MPLE","MM"];
 methods = ["MLE"];
 configGenerator.setLearningMethods(methods);
@@ -102,68 +102,52 @@ PSLModel m = new PSLModel(this, data);
 actions = [1,2,3,4,5];
 actionNames = ["crossing","standing","queueing","walking","talking"];
 
-/* PREDICATES */
-
-// types (might not need some of these)
-//m.add predicate: "frame", types: [ArgumentType.UniqueID];
-//m.add predicate: "bbox", types: [ArgumentType.UniqueID];
-//m.add predicate: "action", types: [ArgumentType.UniqueID];
-
-// target
-m.add predicate: "doing", types: [ArgumentType.UniqueID,ArgumentType.Integer];
-m.add predicate: "sameObj", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
-
-// observed
-m.add predicate: "inFrame", types: [ArgumentType.UniqueID,ArgumentType.Integer,ArgumentType.Integer];
-m.add predicate: "inSameFrame", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
-m.add predicate: "inSeqFrames", types: [ArgumentType.UniqueID,ArgumentType.UniqueID];
-m.add predicate: "dims", types: [ArgumentType.UniqueID,ArgumentType.Integer,ArgumentType.Integer,ArgumentType.Integer,ArgumentType.Integer];
-m.add predicate: "hogAction", types: [ArgumentType.UniqueID,ArgumentType.Integer];
-m.add predicate: "acdAction", types: [ArgumentType.UniqueID,ArgumentType.Integer];
-
-// derived
+// FUNCIONAL PREDICATES
 m.add function: "close", implementation: new ClosenessFunction(1e5, 1e-1);
+
+/* FUNCTIONAL CONSTRAINTS */
+
+// Functional constraint on doing means that it should sum to 1 for each BB
+m.add PredicateConstraint.Functional, on: doing;
+
+// Partial functional constraint on sameObj
+m.add PredicateConstraint.PartialFunctional, on: sameObj;
 
 /* ACTION RULES */
 
 for (int a1 : actions) {
-	// Priors on actions
-//	m.add rule: ~doing(BB1,a1), weight: 1.0, squared: sq;
 
 	// HOG-based SVM probabilities
-	m.add rule: hogAction(BB,a1) >> doing(BB,a1), weight: 1.0, squared: sq;
+//	m.add rule: hogAction(BB,a1) >> doing(BB,a1), weight: 1.0, squared: sq;
 	
 	// ACD-based SVM probabilities
-	m.add rule: acdAction(BB,a1) >> doing(BB,a1), weight: 1.0, squared: sq;
+	m.add rule: acdAction(BB,a1) >> doing(BB,a1), weight: 0.8, squared: sq;
 
-	// Relational rules (same action)
+	// Continuity of actions
+	// If BB1,BB2 (in sequential frames) are the same object, and BB1 is doing action a1, then BB2 is doing action a1.
+	m.add rule: ( sameObj(BB1,BB2) & doing(BB1,a1) ) >> doing(BB2,a1), weight: 1.0, squared: sq;
+
+	// Action transitions
 	for (int a2 : actions) {
-		// If BB1,BB2 in sequential frames, and BB1 is doing action a1, then BB2 is doing action a2.
-		m.add rule: ( sameObj(BB1,BB2) & doing(BB1,a1) ) >> doing(BB2,a2), weight: 1.0, squared: sq;
+		if (a1 == a2) continue;
+		// If BB1,BB2 (in sequential frames) are the same object, and BB1 is doing action a1, then BB2 is doing action a2.
+		m.add rule: ( sameObj(BB1,BB2) & doing(BB1,a1) ) >> doing(BB2,a2), weight: 0.7, squared: sq;
+	}
+
+	// Effect of proximity on actions
+	for (int a2 : actions) {
 		// If BB1,BB2 in same frame, and BB1 is doing action a1, and BB2 is close, then BB2 is doing action a2.
-		m.add rule: ( inSameFrame(BB1,BB2) & doing(BB1,a1) & dims(BB1,X1,Y1,W1,H1) & dims(BB2,X2,Y2,W2,H2) & close(X1,X2,Y1,Y2,W1,W2,H1,H2) ) >> doing(BB2,a2), weight: 1.0, squared: sq;
-		// If BB1,BB2 in same frame, and BB1 is doing action a1, and BB2 is far, then BB2 is doing action a2.
-//		m.add rule: ( inSameFrame(BB1,BB2) & doing(BB1,a1) & dims(BB1,X1,Y1,W1,H1) & dims(BB2,X2,Y2,W2,H2) & ~close(X1,X2,Y1,Y2,W1,W2,H1,H2) ) >> doing(BB2,a2), weight: 1.0, squared: sq;
+		m.add rule: ( inSameFrame(BB1,BB2) & doing(BB1,a1) & dims(BB1,X1,Y1,W1,H1) & dims(BB2,X2,Y2,W2,H2) & close(X1,X2,Y1,Y2,W1,W2,H1,H2) ) >> doing(BB2,a2), weight: 0.2, squared: sq;
 	}
 }
-// Functional constraint on doing means that it should sum to 1 for each BB
-m.add PredicateConstraint.Functional, on: doing;
-
-/* ID MAINTENANCE: IN-FRAME RULES */
-
-// If BB1,BB2 in same frame, cannot be same object.
-//m.add rule: inSameFrame(BB1,BB2) >> ~sameObj(BB1,BB2), constraint: true;
 
 /* ID MAINTENANCE: BETWEEN-FRAME RULES */
 
 // If BB1 in F1, BB2 in F2, and F1,F2 are sequential, and BB1,BB2 are close, then BB1,BB2 are same object.
-m.add rule: ( inSeqFrames(BB1,BB2) & dims(BB1,X1,Y1,W1,H1) & dims(BB2,X2,Y2,W2,H2) & close(X1,X2,Y1,Y2,W1,W2,H1,H2) ) >> sameObj(BB1,BB2), weight: 1.0, squared: sq;
+m.add rule: ( inSeqFrames(BB1,BB2) & dims(BB1,X1,Y1,W1,H1) & dims(BB2,X2,Y2,W2,H2) & close(X1,X2,Y1,Y2,W1,W2,H1,H2) ) >> sameObj(BB1,BB2), weight: 0.25, squared: sq;
 
 // Prior on sameObj
-m.add rule: ~sameObj(BB1,BB2), weight: 1.0, squared: sq;
-
-// Partial functional constraint on sameObj
-m.add PredicateConstraint.PartialFunctional, on: sameObj;
+m.add rule: ~sameObj(BB1,BB2), weight: 0.2, squared: sq;
 
 log.info("Model: {}", m)
 
@@ -172,46 +156,22 @@ Map<CompatibilityKernel,Weight> initWeights = new HashMap<CompatibilityKernel, W
 for (CompatibilityKernel k : Iterables.filter(m.getKernels(), CompatibilityKernel.class))
 	initWeights.put(k, k.getWeight());
 
-/*** LOAD DATA ***/
 
-log.info("Loading data ...");
+/** DATASTORE PARTITIONS **/
 
-/* Create obs/label partitions for each sequence */
 int partCnt = 0;
-Partition[][] partitions = new Partition[2][folds];
-for (int fold = 0; fold < folds; fold++) {
-	partitions[0][fold] = new Partition(partCnt++);	// observations
-	partitions[1][fold] = new Partition(partCnt++);	// labels
+Partition[][] partitions = new Partition[2][numSeqs];
+for (int s = 0; s < numSeqs; s++) {
+	partitions[0][s] = new Partition(partCnt++);	// observations
+	partitions[1][s] = new Partition(partCnt++);	// labels
 }
-
-Inserter[] inserters;
-
-/* Ground truth */
-inserters = InserterUtils.getMultiPartitionInserters(data, doing, partitions[1], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "action.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, sameObj, partitions[1], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "sameObj.txt");
-
-/* Observations */
-inserters = InserterUtils.getMultiPartitionInserters(data, inFrame, partitions[0], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "inframe.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, inSameFrame, partitions[0], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "insameframe.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, inSeqFrames, partitions[0], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "inseqframes.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, dims, partitions[0], folds);
-InserterUtils.loadDelimitedDataMultiPartition(inserters, filePfx + "coords.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, hogAction, partitions[0], folds);
-InserterUtils.loadDelimitedDataTruthMultiPartition(inserters, filePfx + "hogaction.txt");
-inserters = InserterUtils.getMultiPartitionInserters(data, acdAction, partitions[0], folds);
-InserterUtils.loadDelimitedDataTruthMultiPartition(inserters, filePfx + "acdaction.txt");
-
-
+	
+	
 /** GLOBAL DATA FOR DB POPULATION **/
 
 List<List<GroundTerm>> bboxesInSeq = new ArrayList<List<GroundTerm>>();
 List<List<GroundTerm[]>> potentialMatchesInSeq = new ArrayList<List<GroundTerm[]>>();
-for (int s = 0; s < folds; s++) {
+for (int s = 0; s < numSeqs; s++) {
 	bboxesInSeq.add(new ArrayList<GroundTerm>());
 	potentialMatchesInSeq.add(new ArrayList<GroundTerm[]>());
 }
@@ -231,7 +191,7 @@ for (GroundAtom a : atoms) {
 }
 db.close();
 Map<GroundTerm,Integer> actionMap = new HashMap<GroundTerm,Integer>();
-for (int i = 0; i < actions.length; i++) {
+for (int i = 0; i < actions.size(); i++) {
 	actionMap.put(new IntegerAttribute(actions[i]), i);
 }
 
@@ -240,16 +200,17 @@ for (int i = 0; i < actions.length; i++) {
 
 log.info("Starting experiments.");
 
+int numFolds = 44;
+
 Map<String, List<MulticlassPredictionStatistics>> stats_doing = new HashMap<String, List<MulticlassPredictionStatistics>>()
 for (ConfigBundle method : configs)
 	stats_doing.put(method, new ArrayList<MulticlassPredictionStatistics>())
-//Map<String, List<DiscretePredictionStatistics>> stats_sameObj = new HashMap<String, List<DiscretePredictionStatistics>>()
-//for (ConfigBundle method : configs)
-//	stats_sameObj.put(method, new ArrayList<DiscretePredictionStatistics>())
-	
-//for (int fold = 0; fold < folds; fold++) {
-for (int fold = 0; fold < 1; fold++) {
 
+for (int fold = 0; fold < numFolds; fold++) {
+//for (int fold = 0; fold < 2; fold++) {
+	
+	log.info("\n\n*** STARTING FOLD {} ***\n", fold)
+		
 	/** SPLIT DATA **/
 	
 	log.info("Splitting data ...");
@@ -257,14 +218,18 @@ for (int fold = 0; fold < 1; fold++) {
 	/* To construct training set: query for all of the atoms from each scene, except for hold-out. */
 	List<Partition> trainPartsObs = new ArrayList<Partition>();
 	List<Partition> trainPartsLab = new ArrayList<Partition>();
-	for (int s = 0; s < folds; s++) {
-		if (s == fold)
-			continue;
-		trainPartsObs.add(partitions[0][s]);
-		trainPartsLab.add(partitions[1][s]);
+	List<Partition> testPartsObs = new ArrayList<Partition>();
+	List<Partition> testPartsLab = new ArrayList<Partition>();
+	for (int s = 0; s < numSeqs; s++) {
+		if ((s+fold) % numFolds != 0) {
+			trainPartsObs.add(partitions[0][s]);
+			trainPartsLab.add(partitions[1][s]);
+		}
+		else {
+			testPartsObs.add(partitions[0][s]);
+			testPartsLab.add(partitions[1][s]);
+		}
 	}
-	testPartObs = partitions[0][fold];
-	testPartLab = partitions[1][fold];
 	
 	/** POPULATE DB ***/
 
@@ -273,7 +238,7 @@ for (int fold = 0; fold < 1; fold++) {
 	Partition write_tr = new Partition(partCnt++);
 	Partition write_te = new Partition(partCnt++);
 	Database trainDB = data.getDatabase(write_tr, toClose, (Partition[])trainPartsObs.toArray());
-	Database testDB = data.getDatabase(write_te, toClose, testPartObs);
+	Database testDB = data.getDatabase(write_te, toClose, (Partition[])testPartsObs.toArray());
 
 	/* Populate doing predicate. */
 	Variable BBox = new Variable("BBox");
@@ -283,8 +248,8 @@ for (int fold = 0; fold < 1; fold++) {
 	// Get all bbox ground terms
 	Set<GroundTerm> bboxTerms_tr = new HashSet<GroundTerm>();
 	Set<GroundTerm> bboxTerms_te = new HashSet<GroundTerm>();
-	for (int s = 0; s < folds; s++) {
-		Set<GroundTerm> curSet = (s != fold) ? bboxTerms_tr : bboxTerms_te;
+	for (int s = 0; s < numSeqs; s++) {
+		Set<GroundTerm> curSet = ((s+fold) % numFolds != 0) ? bboxTerms_tr : bboxTerms_te;
 		curSet.addAll(bboxesInSeq[s]);
 	}
 	// Training
@@ -299,8 +264,8 @@ for (int fold = 0; fold < 1; fold++) {
 	
 	/* Populate sameObj predicate.*/
 	int numTestEx_sameObj = 0;
-	for (int s = 0; s < folds; s++) {
-		if (s != fold) {
+	for (int s = 0; s < numSeqs; s++) {
+		if ((s+fold) % numFolds != 0) {
 			for (GroundTerm[] terms : potentialMatchesInSeq[s]) {
 				RandomVariableAtom rv = (RandomVariableAtom)trainDB.getAtom(sameObj, terms[0], terms[1]);
 				trainDB.commit(rv);
@@ -320,6 +285,7 @@ for (int fold = 0; fold < 1; fold++) {
 	
 	/* Label DBs */
 	Database labelDB = data.getDatabase(new Partition(partCnt++), [doing,sameObj] as Set, (Partition[])trainPartsLab.toArray());
+	Database truthDB = data.getDatabase(new Partition(partCnt++), [doing,sameObj] as Set, (Partition[])testPartsLab.toArray());
 
 	/*** EXPERIMENT ***/
 	
@@ -332,11 +298,10 @@ for (int fold = 0; fold < 1; fold++) {
 
 		/* Weight learning */
 		WeightLearner.learn(method, m, trainDB, labelDB, initWeights, config, log)
-
 		log.info("Learned model {}: \n {}", configName, m.toString())
 
 		/* Inference on test set */
-		testDB = data.getDatabase(write_te, toClose, testPartObs);
+		testDB = data.getDatabase(write_te, toClose, (Partition[])testPartsObs.toArray());
 		Set<GroundAtom> targetAtoms = Queries.getAllAtoms(testDB, doing)
 		for (RandomVariableAtom rv : Iterables.filter(targetAtoms, RandomVariableAtom))
 			rv.setValue(0.0)
@@ -350,43 +315,55 @@ for (int fold = 0; fold < 1; fold++) {
 	
 		/* Evaluate doing predicate */
 		Database predDB = data.getDatabase(write_te, [doing,sameObj] as Set);
-		Database truthDB = data.getDatabase(testPartLab, [doing,sameObj] as Set);
 		def comparator = new MulticlassPredictionComparator(predDB);
 		comparator.setBaseline(truthDB);
 		MulticlassPredictionStatistics stats = comparator.compare(doing, actionMap, 1);
 		log.info("F1  ACTION: {}", stats.getF1());
 		log.info("Acc ACTION: {}", stats.getAccuracy());
+		ConfusionMatrix conMat = stats.getConfusionMatrix();
+		System.out.println("Confusion Matrix:\n" + conMat.toMatlabString());
+		System.out.println("Precision matrix:\n" + conMat.getPrecisionMatrix().toMatlabString(3));
 		stats_doing.get(config).add(fold, stats);
-//		def comparator = new DiscretePredictionComparator(predDB);
-//		comparator.setBaseline(truthDB);
-//		comparator.setResultFilter(new MaxValueFilter(doing, 1));
-//		comparator.setThreshold(Double.MIN_VALUE) // treat best value as true as long as it is nonzero
-//		DiscretePredictionStatistics stats = comparator.compare(doing, numTestEx_doing);
-//		System.out.println("F1  Action:  " + stats.getF1(DiscretePredictionStatistics.BinaryClass.POSITIVE));
-//		System.out.println("Acc Action:  " + stats.getAccuracy());
-//		stats_doing.get(config).add(fold, stats);
-//		/* Evaluate doing predicate */
-//		comparator = new DiscretePredictionComparator(predDB);
-//		comparator.setBaseline(truthDB);
-//		comparator.setThreshold(0.5);
-//		stats = comparator.compare(doing, numTestEx_sameObj);
-//		System.out.println("F1 SameObj:  " + stats.getF1(DiscretePredictionStatistics.BinaryClass.POSITIVE));
-//		stats_sameObj.get(config).add(fold, stats)
 		predDB.close();
-		truthDB.close();
 	}
 	
 	/* Close all databases. */
 	trainDB.close();
 	labelDB.close();
+	truthDB.close();
+	
+	/* Empty the write partitions */
+	data.deletePartition(write_tr);
+	data.deletePartition(write_te);
 }
 
-//log.warn("\n\nRESULTS\n");
-//for (ConfigBundle config : configs) {
-//	def configName = config.getString("name", "")
-//	def scores = stats_doing.get(config);
-//	for (int fold = 0; fold < folds; fold++) {
-//		def score = scores.get(fold)
-//		log.warn("{} \t{}\t{}\t{}", configName, fold, score[0], score[1]);
-//	}
-//}
+log.info("\n\nRESULTS\n");
+for (ConfigBundle config : configs) {
+	String configName = config.getString("name", "")
+	def stats = stats_doing.get(config);
+	double avgF1 = 0.0;
+	double avgAcc = 0.0;
+	List<ConfusionMatrix> cmats = new ArrayList<ConfusionMatrix>();
+//	List<SquareMatrix> pmats = new ArrayList<SquareMatrix>();
+	for (int fold = 0; fold < stats.size(); fold++) {
+		avgF1 += stats.get(fold).getF1();
+		avgAcc += stats.get(fold).getAccuracy();
+		ConfusionMatrix cmat = stats.get(fold).getConfusionMatrix();
+		cmats.add(cmat)
+//		pmats.add(cmat.getPrecisionMatrix());
+	}
+	/* Average statistics */
+	avgF1 /= stats.size();
+	avgAcc /= stats.size();
+	log.info("\n{}\n Avg F1: {}\n Avg Acc: {}\n", configName, avgF1, avgAcc);
+//	SquareMatrix avgPMat = SquareMatrix.average(pmats);
+//	log.info("\nAvg Precision Matrix:\n{}", avgPMat.toMatlabString(3));
+	/* Cummulative statistics */
+	ConfusionMatrix cumCMat = ConfusionMatrix.aggregate(cmats);
+	def cumStats = new MulticlassPredictionStatistics(cumCMat);
+	log.info("\n{}\n Cumm F1: {}\n Cumm Acc: {}\n", configName, cumStats.getF1(), cumStats.getF1());
+	log.info("\nCumm Precision Matrix:\n{}", cumCMat.getPrecisionMatrix().toMatlabString(3));
+}
+
+
+
