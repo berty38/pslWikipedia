b48117ccc6619d7e7ce28838b0545d9b99f9a904
Ben London <blondon@malibu.umiacs.umd.edu>
2013-04-15 21:57:17 -0400

    added run script

---
+++ runAction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mvn compile
+
+mvn dependency:build-classpath -Dmdep.outputFile=classpath.out
+
+java -Xmx8g -cp ./target/classes:`cat classpath.out` edu.umd.cs.linqs.action.Action1_DataLoader
+java -Xmx32g -cp ./target/classes:`cat classpath.out` edu.umd.cs.linqs.action.ActionRecog1
